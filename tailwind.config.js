ad3fa20005ddf12766e1f8a5a8ed59cbb3302710
Ruspan Manurung <ruspan.manurung@gmail.com>
2025-11-13 10:02:45 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        orkablack: "#0a0a0a",
+        orkayellow: "#f8e37a",
+      },
+    },
+  },
+  plugins: [],
+};
